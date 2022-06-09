26e4d45a0c793c3f3c5715a23b17eeab0aeae52d
NikshetSteh <88205797+NikshetSteh@users.noreply.github.com>
2022-06-09 23:39:00 +0500

    fixed new new bug

--- main.py
+++ main.py
@@ -79,8 +79,8 @@ else:
         skip_updates = True,
         on_startup = on_startup,
         on_shutdown = on_shutdown,
-        host = WEBAPP_HOST,
-        port = WEBAPP_PORT,
+        host = config.WEBAPP_HOST,
+        port = config.WEBAPP_PORT,
     )
 
 loader.Loader.save(users.values())
